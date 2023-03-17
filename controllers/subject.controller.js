faeac63f9e06cc317ea9a172cd07203b762514bb
RaedBouafif <raed.boafif@gmail.com>
2023-03-17 13:00:37 +0100

    before-pull

--- controllers/subject.controller.js
+++ controllers/subject.controller.js
@@ -1,6 +1,8 @@
 const { default: mongoose } = require('mongoose')
 const { Subject } = require('../models/subject.model')
 const Section = require('../models/section.model')
+const TeacherModel = require("../models/Users/teacher.model");
+
 
 
 //need update
@@ -154,69 +156,37 @@ exports.findSubjectByName = (req, res) => {
 //update subject 
 exports.updateSubject = async (req, res) => {
     try {
-        const subjectName = req.params.subjectName
-        if (!subjectName) {
+        const subjectId = req.params.subjectId
+        if (!subjectId) {
             return res.status(500).send({
                 error: "Bad Request!"
             })
         }
-        if (!req.body.properties) {
-            const subject = await Subject.findOne({ subjectName: subjectName })
-            req.body = { ...req.body, properties: subject.properties }
-        } else {
-            for (let x = 0; x < req.body.properties.length; x++) {
-                let currentSectionName = req.body.properties[x].sectionName
-                const section = await Section.findOne({ sectionName: currentSectionName })
-                if (!section) {
-                    return res.status(400).send({
-                        error: "Section " + currentSectionName + " cannot be added or updated"
-                    })
-                }
-            }
-        }
-        for (let i = 0; i < req.body.properties.length; i++) {
-            for (let j = i + 1; j < req.body.properties.length; j++) {
-                if (req.body.properties[i].sectionName == req.body.properties[j].sectionName) {
-                    return res.status(400).send({
-                        error: "Can not insert a duplicated section in the same subject!"
-                    })
-                }
-            }
-        }
-        Subject.findOneAndUpdate({ subjectName: subjectName }, req.body, { new: true, runValidators: true }).then(subject => {
+        Subject.findByIdAndUpdate(subjectId, req.body, { new: true, runValidators: true }).then(subject => {
             if (!subject) {
                 return res.status(404).send({
-                    message: "Subject with name : " + subjectName + " Not found!",
+                    message: "Subject with id : " + subjectName + " Not found!",
                     updated: false
                 })
             }
+            console.log(subject)
             return res.status(200).send({
                 subject,
                 updated: true
             })
         }).catch(err => {
-            if (err instanceof mongoose.Error.ValidationError) {
-                const errorMessages = []
-                for (let path in err.errors) {
-                    if (err.errors.hasOwnProperty(path)) {
-                        errorMessages.push(err.errors[path].message)
-                    }
-                }
-                return res.status(400).send({
-                    error: errorMessages.join(', ') + "|| for subject name :" + subjectName
-                })
-            }
-            if (err.kind = 'ObjectId') {
-                return res.status(400).send({
-                    error: "Coefficient should be a number"
-                })
-            }
             return res.status(400).send({
                 error: err.message,
-                message: "Some error occured while updating the subject with name " + subjectName
+                message: "Some error occured while updating the subject with id " + subjectId
             })
         })
     } catch (e) {
+        if (e.keyValue?.subjectName) {
+            return res.status(409).send({
+                error: "Bad Credentials!",
+                message: "Subject allready exists!"
+            })
+        }
         return res.status(500).send({
             error: e.message,
             message: "Server error!"
@@ -227,32 +197,35 @@ exports.updateSubject = async (req, res) => {
 //delete a subject
 exports.deleteSubject = (req, res) => {
     try {
-        const { subjectName } = req.params
-        if (!subjectName) {
+        const { subjectId } = req.params
+        if (!subjectId) {
             return res.status(400).send({
                 error: "Bad Request!"
             })
         }
-        Subject.findOneAndDelete({ subjectName: subjectName }).then(subject => {
+        Subject.findByIdAndDelete(subjectId).then( (subject) => {
             if (!subject) {
                 return res.status(404).send({
-                    message: "Subject with name " + subjectName + "Not found!",
+                    message: "Subject with id " + subjectId + "Not found!",
                     deleted: false
                 })
             }
+            subject.teachers.forEach( async (teacher,index) => {
+                await TeacherModel.findByIdAndUpdate(teacher[index], { $pull : { $subjects :subject._id }}, { runValidators: true, new: true })
+            })
             return res.status(200).send({
-                message: "Subject with the name " + subjectName + " Successfully deleted",
+                message: "Subject with the id " + subjectId + " Successfully deleted",
                 deleted: true
             })
         }).catch(err => {
             if (err.kind === 'ObjectId' || err.name === 'NotFound') {
                 return res.status(404).send({
-                    error: "Subject with the name " + subjectName + "not found!"
+                    error: "Subject with the id " + subjectId + "not found!"
                 })
             }
             return res.status(400).send({
                 error: err.message,
-                message: "Some error occured while deleting the subject with name " + subjectName
+                message: "Some error occured while deleting the subject with id " + subjectId
             })
         })
     } catch (e) {
